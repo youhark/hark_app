61fa384b82d71861a4f79a8bcfabc004cf951056
vagrant <vagrant@precise32.(none)>
2015-08-29 18:15:32 +0000

    working on home page

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@ Rails.application.routes.draw do
              :controllers => { omniauth_callbacks: 'omniauth_callbacks', registrations: 'registrations' }
   post '/signup' => 'splash#signup', as: :splash_signup
   get '/splash' => 'splash#index'
-  root 'splash#index'
+  root 'home#index'
   get '/setup' => 'setup#index'
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
